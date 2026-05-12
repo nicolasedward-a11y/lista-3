7b561b952fad5f5a9668bd3752e064f8a395a673
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 18:40:43 -0300

    resolvi exercicio 1

--- src/has_cycle.py
+++ src/has_cycle.py
@@ -2,4 +2,17 @@ from src.my_node import MyNode
 
 
 def has_cycle(head: MyNode) -> bool:
-    raise NotImplementedError
+    if head is None:
+        return False
+    
+    slow = head
+    fast = head
+    
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        
+        if slow == fast:
+            return True
+        
+    return False 
