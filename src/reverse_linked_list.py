a755c511dac0e4088062ac4bbde7c330c6b5df12
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 19:20:04 -0300

    Resolvido

--- src/reverse_linked_list.py
+++ src/reverse_linked_list.py
@@ -2,4 +2,14 @@ from src.my_node import MyNode
 
 
 def reverse_linked_list(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    anterior = None
+    atual = head
+
+    while atual is not None:
+        proximo = atual.next
+        atual.next = anterior
+
+        anterior = atual
+        atual = proximo
+
+    return anterior
