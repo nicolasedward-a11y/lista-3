43be306794f49bf020199a52b8ef8b86ae85e8f6
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 19:04:55 -0300

    Respondi questões 2 e 3

--- src/has_cycle.py
+++ src/has_cycle.py
@@ -2,17 +2,14 @@ from src.my_node import MyNode
 
 
 def has_cycle(head: MyNode) -> bool:
-    if head is None:
-        return False
-    
-    slow = head
-    fast = head
-    
-    while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
-        
-        if slow == fast:
+    lento = head
+    rapido = head
+
+    while rapido is not None and rapido.next is not None:
+        lento = lento.next
+        rapido = rapido.next.next
+
+        if lento == rapido:
             return True
-        
-    return False 
+
+    return False
--- src/merge_lists.py
+++ src/merge_lists.py
@@ -2,4 +2,34 @@ from src.my_node import MyNode
 
 
 def merge_lists(lista1: MyNode, lista2: MyNode) -> MyNode:
-    raise NotImplementedError
+    if lista1 is None:
+        return lista2
+
+    if lista2 is None:
+        return lista1
+
+    if lista1.value < lista2.value:
+        head = lista1
+        lista1 = lista1.next
+    else:
+        head = lista2
+        lista2 = lista2.next
+
+    atual = head
+
+    while lista1 is not None and lista2 is not None:
+        if lista1.value < lista2.value:
+            atual.next = lista1
+            lista1 = lista1.next
+        else:
+            atual.next = lista2
+            lista2 = lista2.next
+
+        atual = atual.next
+
+    if lista1 is not None:
+        atual.next = lista1
+    else:
+        atual.next = lista2
+
+    return head
